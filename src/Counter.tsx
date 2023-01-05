15e6593cfed6d79422207bbdea5dc99074c731f9
honey mittal <hellohoneymittal@gmail.com>
2023-01-05 20:01:34 +0530

    remove useeffect

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useAppDispatch, useAppSelector } from "./Selectors/hooks";
 import { RootState } from "./Selectors/store";
 import { increment, decrement } from "./Selectors/todoSlice";
